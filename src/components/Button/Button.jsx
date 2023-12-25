86c8fdd70df3ad795f06210e01c24f8e5686b6fa
Ahmed Gamal <ahmedgamal.ssaleh@gmail.com>
2023-12-25 22:57:05 +0200

    change button styling

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,8 +1,19 @@
 import {BASE_URL} from "../../data.js";
+
 const Button = ({text, gid}) => {
+
+    const handleMouseOver = (e) => {
+        e.target.className = "";
+    }
+
+    const handleMouseOut = (e) => {
+        e.target.className = "outline";
+    }
+
     return (
-        <a href={`${BASE_URL + gid}`} target="_blank" rel="noreferrer" role={"button"}>
-                {text}
+        <a href={`${BASE_URL + gid}`} target="_blank" rel="noreferrer" role={"button"} className={"outline"}
+           onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
+            {text}
         </a>
     )
 }
